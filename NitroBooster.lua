e4a3d5b4eaa60c1c2a9a44f294224c23970b7724
Björn Geschka <bjoern@geschka.org>
2025-10-10 21:02:39 +0200

    Initial version

---
+++ NitroBooster.lua
@@ -0,0 +1,294 @@
+local NITRO_SPELLID = 54861
+local NB_BOOTS_MAX = 5
+
+function nb_boots_get_config(ctx)
+	-- local name = GetUnitName("Player");
+	-- nb_load_boots_from_gui(ctx);
+	local conf = bg_config_get(ctx, "boots");
+	if not conf then
+		bg_dbg(ctx, "empty config, creating new");
+		bg_config_set(ctx, "boots", {})
+		conf = bg_config_get(ctx, "boots");
+	end
+	return conf;
+end
+
+
+function nb_item_get_state(ctx, itemid)
+	local ctime = GetTime();
+	local equipped_boots = GetInventoryItemID("player", 8)
+	local equipped = itemid == equipped_boots;
+	local started = 0;
+	local duration = 0;
+	local enable = 0;
+	if equipped_boots == itemid then
+		started, duration, enable = GetItemCooldown(itemid)
+	else
+		local bag, slot = bg_bag_find_item(itemid);
+		if bag == nil or slot == nil then
+			bg_dbg(ctx, "something went wrong bag index/slot index getting status of "..itemid);
+			return
+		else
+			started, duration, enable = GetContainerItemCooldown(bag, slot)
+		end
+	end
+
+	local cd_left = (started+duration)-ctime;
+	if started == 0 then
+		cd_left = 0
+	end
+
+	-- boots in bag and unused, but equipping will trigger cd
+	if not equipped and cd_left < 30 then
+		cd_left = 30
+	end
+
+
+	return {
+		cd_left = cd_left,
+		equipped = equipped,
+		usable = (itemid == equipped_boots) and cd_left <= 0
+	}
+end
+
+function nb_del_boots(ctx, itemid)
+	local boots = nb_boots_get_config(ctx);
+	bg_dbg(ctx, "deleting boots:" .. itemid);
+	boots[itemid] = nil ;-- nb_item_get_cd(ctx, itemid)
+end
+
+
+function nb_add_boots(ctx, itemid, prio, link)
+	local boots = nb_boots_get_config(ctx);
+	if boots[itemid] then
+		bg_dbg(ctx, "boots already there: " .. itemid);
+		return
+	end
+	bg_dbg(ctx, "adding boots:" .. itemid);
+	boots[itemid] = {
+		prio = prio,
+		link = link
+	} ;-- nb_item_get_cd(ctx, itemid)
+end
+
+function nb_add_boots_test(ctx)
+	nb_add_boots(ctx, 49907) -- [Boots of Kingly Upheaval]
+	nb_add_boots(ctx, 40742) -- [Bladed Steelboots]
+	nb_add_boots(ctx, 40743) -- [Kyzoc's Ground Stompers]
+	nb_add_boots(ctx, 45166) -- [Charred Saronite Greaves]
+	nb_add_boots(ctx, 47885) -- [Greaves of the Lingering Vortex]
+end
+
+function nb_boots_update(ctx)
+	local ctime = GetTime();
+	local boots = nb_boots_get_config(ctx);
+	for k, v in pairs(boots) do
+		local prio = boots[k].prio;
+		local link = boots[k].link;
+
+		boots[k] = nb_item_get_state(ctx, k)
+
+		boots[k].prio = prio
+		boots[k].link = link
+	end
+end
+
+function nb_get_equipped(ctx)
+	local boots = nb_boots_get_config(ctx);
+	for k, v in pairs(boots) do
+		if v.equipped == true then
+			return v
+		end
+	end
+	return nil
+end
+
+function PlayerHasBuff(buffName)
+	local i = 1
+	while true do
+		local name = UnitBuff("player", i)
+		if not name then
+			break -- No more buffs
+		end
+		if name == buffName then
+			return true
+		end
+		i = i + 1
+	end
+	return false
+end
+
+function nb_next(ctx)
+
+	if UnitAffectingCombat("player") then
+		-- print("cannot change in combat");
+		return false;
+	end
+
+	-- if PlayerHasBuff("Nitro Boosts") then
+	--	-- print("already boooosting");
+	--	return false;
+	-- end
+
+	local lowest_cd = 300;
+	local lowest_cd_item = "";
+	local equipped = false;
+	local boots = nb_boots_get_config(ctx);
+	if bg_table_len(boots) < 1 then
+		-- print("no boots");
+		return
+	end
+
+	-- local prio = 0;
+	for k, v in pairs(boots) do
+		if ( boots[k].cd_left + boots[k].prio ) < lowest_cd then
+			lowest_cd = boots[k].cd_left
+			equipped  = boots[k].equipped
+			lowest_cd_item = k
+			lowest_cd_item_link = boots[k].link
+		end
+	end
+
+	if equipped then
+		return false;
+	end
+	bg_dbg(ctx, "equipping :: next lowest cd has:"..lowest_cd_item_link .. " cd:"..lowest_cd);
+	bg_equip_item_by_id(lowest_cd_item);
+end
+
+function nb_config_name_by_idx_gui(idx)
+	return bg_playername() .. "gui_nitro_boots_" .. idx
+end
+
+function nb_load_boots_from_gui(ctx)
+	-- return just itemids in table
+	bg_config_set(ctx, "boots", {});
+	local ret = {};
+	for idx=1,NB_BOOTS_MAX do
+		local itemLink = bg_config_get(ctx, nb_config_name_by_idx_gui(idx));
+		if itemLink then
+			local itemID = tonumber(strmatch(itemLink, "item:(%d+)"))
+			if itemID then
+				print("adding boots:"..itemLink);
+				nb_add_boots(ctx, itemID, idx, itemLink);
+			end
+		end
+	end
+end
+
+local gui_built = false
+function nb_gui(ctx)
+	local topcontainer = bg_frame_main_get(ctx);
+	if not gui_built then
+		bg_frame_main_add_config_CheckBox(ctx, "Enable auto-swap", "enabled");
+		for idx=1,NB_BOOTS_MAX do
+			bg_frame_main_add_config_EditBox(ctx, "pair_"..idx , nb_config_name_by_idx_gui(idx), nil, function()
+				nb_load_boots_from_gui(ctx);
+			end)
+		end
+		bg_frame_main_add_config_EditBox(ctx, "Interval in seconds/fractions (needs ui reload, default 0.1)", "interval")
+		bg_frame_main_add_config_CheckBox(ctx, "Debug (needs ui reload)", "debug");
+		gui_built = true
+	end
+
+	topcontainer:Show();
+end
+
+function nb_config_get_interval(ctx)
+	local default = 0.1
+	local interval= default 
+	if bg_config_get(ctx, "interval") then
+		interval = tonumber(bg_config_get(ctx, "interval"))
+		if not interval  then
+			interval = default
+		end
+	end
+	return interval
+end
+
+function nb_print_help()
+	print("Welcome to Nitrobooster!")
+	print(" /nb gui -- show config window")
+	print(" /nb list -- show current boots")
+	print(" /nb reset -- resets the configuration");
+end
+
+bg_context_new("NitroBooster", "NitroBoosterConfig", function(ctx)
+	ctx.debug = bg_config_get(ctx, "debug");
+	bg_slashcommand(ctx, "/nb", function (param1, param2, param3)
+		local boots = nb_boots_get_config(ctx);
+
+		if param1 == "test" then
+			nb_add_boots_test(ctx);
+			return false;
+		end
+		if param1 == "update" then
+			nb_boots_update(ctx);
+			return false;
+		end
+		if param1 == "add" then
+			nb_add_boots(ctx,tonumber(param2));
+			bg_dbg_table(ctx, boots, 2)
+			return false;
+		end
+		if param1 == "del" then
+			nb_del_boots(ctx,tonumber(param2));
+			bg_dbg_table(ctx, boots, 2)
+			return false;
+		end
+		if param1 == "reset" then
+			-- bg_config_set(ctx, "boots", {});
+			bg_config_wipe(ctx);
+			return false;
+		end
+		if param1 == "gui" or param1 == "conf" or param1 == "config" then
+			nb_gui(ctx);
+			return false;
+		end
+		if param1 == "dump" then
+			-- bg_dump_config(ctx);
+			return false;
+		end
+		if param1 == "loadgui" then
+			nb_load_boots_from_gui(ctx);
+			return false;
+		end
+		if param1 == "list" then
+			bg_dbg(ctx, "listing boots>>>>");
+			bg_dbg_table(ctx, boots, 2);
+			bg_dbg(ctx, "done listing boots<<<<");
+			return false;
+		end
+
+		-- equip the next pair of boots that are ready to use
+		if param1 == "next" then
+			nb_next(ctx);
+			return false;
+		end
+		-- if param1 == "run" then
+		--	bh_run(ctx);
+		--	return false;
+		-- end
+		-- print("hi");
+		-- bg_dbg(ctx, "hiiii!");
+		--
+		nb_print_help();
+	end);
+
+	bg_timer_interval(ctx, nb_config_get_interval(ctx), function (ctx)
+		local enabled = bg_config_get(ctx, "enabled")
+		if enabled then
+			nb_boots_update(ctx);
+			nb_next(ctx);
+		end
+	end);
+
+	nb_load_boots_from_gui(ctx);
+
+	-- bg_event_handler_add(ctx, "OnUpdate", function(ctx)
+	--	print("update?! " .. GetTime())
+	-- end)
+	--
+	-- nb_add_boots_test(ctx);
+
+end);
---
+++ lib_bg2.lua
@@ -0,0 +1,305 @@
+-- https://www.wowace.com/projects/ace3/pages/ace-gui-3-0-widgets
+
+
+function bg_dbg(ctx, ...)
+	if ctx.debug then
+		-- local msg = ""
+		-- for i,v in ipairs(arg) do
+		--	msg = msg .. tostring(v) .. " "
+		-- end
+		print("[[" .. ctx.addonname .. "]] ", ...)
+	end
+end
+
+function bg_playername()
+	return GetUnitName("PLAYER")
+end
+
+function bg_event_handler_add(ctx, eventname, callback)
+	bg_dbg(ctx, "registering eventhandlers", eventname);
+	ctx.eventhandlers[eventname] = callback;
+	ctx.frame_conv:RegisterEvent(eventname);
+end
+
+local timers = 0
+function bg_timer_interval(ctx, interval_seconds, callback)
+	timers = timers + 1;
+	ctx.timers["u_"..timers] = {
+		lastcall = 0,
+		interval = interval_seconds,
+		callback = callback
+	}
+end
+
+function bg_context_new(addonname, configname, readycallback)
+	local ctx = {
+		addonname = addonname,
+		configname = configname,
+		loaded = false,
+		debug = false,
+		frame_conv = nil,
+		frame_main = nil,
+		eventhandlers = {},
+		timers = {}
+	}
+	ctx.frame_conv = CreateFrame("Frame", addonname .."FrameForAddonLoad", UIParent);
+	ctx.frame_conv:SetScript("OnUpdate", function(event, eventname, ...)
+		-- print("updating:"..GetTime().. " event " .. eventname)
+		local ctime = GetTime();
+		for k, v in pairs(ctx.timers) do
+			if (v.lastcall + v.interval) < ctime then
+				v.callback(ctx);
+				v.lastcall = ctime
+			end
+		end
+	end)
+
+	ctx.frame_conv:SetScript("OnEvent", function(event, eventname, ...)
+		-- bg_dbg(ctx, "event:", eventname);
+		if ctx.eventhandlers[eventname] then
+			-- bg_dbg(ctx,"calling handler:", eventname);
+			ctx.eventhandlers[eventname](...)
+		end
+	end);
+
+	bg_event_handler_add(ctx, "ADDON_LOADED", function (...)
+		if(ctx.loaded) then
+			return;
+		end
+		ctx.loaded = true
+		if _G[ctx.configname] == nil then
+			_G[ctx.configname] = {
+				-- empty storage object
+			}
+		end
+		readycallback(ctx);
+	end);
+	return ctx;
+end
+
+function bg_dump_config(ctx)
+	bg_dbg_table(_G[ctx.configname]);
+end
+
+-- my own lib, convenience functions
+function bg_string_split(inputstr, sep)
+	if sep == nil then
+		sep = "%s"
+	end
+	local t = {}
+	for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+		table.insert(t, str)
+	end
+	return t
+end
+
+bg_CLASSCOLORS = {
+	DEATHKNIGHT	= {R = 0.77	,G = 0.12	,B = 0.23},
+	DRUID		= {R = 1	,G = 0.49	,B = 0.04},
+	HUNTER		= {R = 0.67	,G = 0.83	,B = 0.45},
+	MAGE		= {R = 0.41	,G = 0.8	,B = 0.94},
+	PALADIN		= {R = 0.96	,G = 0.55	,B = 0.73},
+	PRIEST		= {R = 1	,G = 1		,B = 1},
+	ROGUE		= {R = 1	,G = 0.96	,B = 0.41},
+	SHAMAN		= {R = 0	,G = 0.44	,B = 0.87},
+	WARLOCK		= {R = 0.58	,G = 0.51	,B = 0.79},
+	WARRIOR		= {R = 0.78	,G = 0.61	,B = 0.43}
+};
+
+
+function bg_slashcommand(ctx, slashcmd, cb)
+	local str_globvar = "SLASH_"..ctx.addonname.."1"
+	_G[str_globvar] = slashcmd;
+
+	SlashCmdList[ctx.addonname] = function(msg)
+		local t = bg_string_split(msg, " ");
+		local param1 = t[1];
+		local param2 = t[2];
+		local param3 = t[3];
+		cb(param1, param2, param3);
+	end
+end;
+
+
+function bg_dbg_table (ctx, tbl, indent)
+	if not indent then indent = 0 end
+	for k, v in pairs(tbl) do
+		formatting = string.rep("  ", indent) .. k .. ": "
+		if type(v) == "table" then
+			bg_dbg(ctx, formatting)
+			bg_dbg_table(ctx, v, indent+1)
+		elseif type(v) == 'boolean' then
+			bg_dbg(ctx,formatting .. tostring(v))
+		else
+			bg_dbg(ctx,formatting .. v)
+		end
+	end
+end
+
+function bg_frame_main_get(ctx)
+	if ctx.frame_main == nil then
+		local AceGUI = LibStub("AceGUI-3.0")
+		ctx.AceGUI = AceGUI;
+		local frame = AceGUI:Create("Frame")
+		frame:SetTitle(ctx.addonname)
+		frame:SetStatusText("made by pewz")
+		--[[
+		container:SetLayout(layout)
+		Set the Layout this container should use when managing its child frames.
+		Currently implemented Layouts in AceGUI-3.0:
+		"Flow" - A row based flow layout
+		"List" - A simple stacking layout
+		"Fill" - Fill the whole container with the first widget (used by Groups)
+		--]]
+
+		frame:SetLayout("List")
+		-- frame:SetLayout("Fill")
+
+		ctx.frame_main = frame;
+	end
+
+	return ctx.frame_main;
+end
+
+function bg_bag_qty_by_link(link)
+	local qty = 0;
+	for i=0,NUM_BAG_SLOTS do
+		for j=1,GetContainerNumSlots(i) do
+			if GetContainerItemLink(i,j)==link then
+				local icon, itemCount, locked, quality, readable, lootable, itemLink, isFiltered, noValue, ItemID, isBound = GetContainerItemInfo(i, j)
+				if itemCount ~= nil then
+					qty = qty + itemCount;
+				end
+			end
+		end
+	end
+
+	return qty
+end
+
+function bg_bag_qty_by_id(ItemID)
+	local itemName,link = GetItemInfo(ItemID);
+	return bg_bag_qty_by_link(link);
+end
+
+function bg_bag_qty_by_id_enough(why ,ItemID, qty)
+	local itemName,link=GetItemInfo(ItemID);
+	local havqty = bg_bag_qty_by_link(link)
+	if havqty >= qty then return true end
+	print("not enough "..itemName .. " for " .. why .. " (x"..(qty - havqty)..")");
+	return false;
+end
+
+function bg_config_print(ctx)
+	print("config for:"..ctx.addonname);
+	print("------------------------------");
+	for k,v in pairs(_G[ctx.configname]) do
+		print(" >> " .. k.." : " .. tostring(v));
+	end
+	print("------------------------------");
+end
+
+function bg_config_wipe(ctx)
+	_G[ctx.configname] = {};
+end
+
+function bg_config_set(ctx, key, value)
+	_G[ctx.configname][key] = value;
+	if ctx.debug then
+		bg_config_print(ctx);
+	end
+end
+
+function bg_config_get(ctx, key)
+	local value = _G[ctx.configname][key];
+	return value
+end
+
+function bg_frame_main_add_config_EditBox(ctx, label, configfield, parent, cb)
+	local eb = ctx.AceGUI:Create("EditBox")
+	eb:SetLabel(label)
+	eb:SetWidth(400)
+	local startvalue = bg_config_get(ctx, configfield);
+	if startvalue then
+		eb:SetText(startvalue);
+	end
+	-- eb:DisableButton(true)
+	eb:SetCallback("OnEnterPressed", function(widget, event, text)
+		bg_config_set(ctx, configfield, text);
+		if cb then
+			cb()
+		end
+		-- widget:ClearFocus();
+		return true;
+	end)
+
+
+	if parent then
+		parent:AddChild(eb)
+	else
+		ctx.frame_main:AddChild(eb)
+	end
+	return eb;
+end
+
+function bg_frame_main_add_config_CheckBox(ctx, label, configfield)
+	local config_checkbox = ctx.AceGUI:Create("CheckBox")
+	config_checkbox:SetLabel(label)
+	config_checkbox:SetWidth(400)
+	config_checkbox:SetValue(false); --always start false
+	local startvalue = bg_config_get(ctx, configfield);
+	if startvalue == true then
+		config_checkbox:SetValue(true);
+	end
+	config_checkbox:SetCallback("OnValueChanged", function(self, event, value)
+		bg_config_set(ctx, configfield, value);
+	end);
+	ctx.frame_main:AddChild(config_checkbox)
+end
+
+function bg_bag_for_each(cb)
+	for i=0,NUM_BAG_SLOTS do
+		for j=1,GetContainerNumSlots(i) do
+			cb(i,j);
+		end
+	end
+end
+
+function bg_string_append(str, append)
+	return str .. append
+end
+
+function bg_string_append_nl(str, append)
+	return bg_string_append(str,bg_string_append("\n",append));
+end
+
+function bg_bag_find_item(itemID)
+	for bag = 0, 4 do
+		local numSlots = GetContainerNumSlots(bag)
+		for slot = 1, numSlots do
+			local itemLink = GetContainerItemLink(bag, slot)
+			if itemLink then
+				local foundID = tonumber(string.match(itemLink, "item:(%d+):"))
+				if foundID == itemID then
+					return bag, slot
+				end
+			end
+		end
+	end
+	return nil -- Not found
+end
+
+function bg_equip_item_by_id(itemID)
+	local bag, slot = bg_bag_find_item(itemID)
+	if bag and slot then
+		UseContainerItem(bag, slot)
+		return true -- Success
+	end
+	return false -- Not found or failed
+end
+
+function bg_table_len(T)
+	local count = 0
+	for _ in pairs(T) do count = count + 1 end
+	return count
+end
